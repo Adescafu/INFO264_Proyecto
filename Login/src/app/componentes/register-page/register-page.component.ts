7760be515246355f93f8efd7a4277f51f386a533
Blacky91 <juancontre91@gmail.com>
2018-12-15 23:15:26 -0300

    Ya sin error

--- Login/src/app/componentes/register-page/register-page.component.ts
+++ Login/src/app/componentes/register-page/register-page.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import{ AuthService } from '../../servicios/auth.service';
+ 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent implements OnInit {
-
-  constructor() { }
+  public email: string;
+  public password: string;
+  constructor(
+    public authService: AuthService
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSubmitAddUsser(){
+    this.authService.registrarusuario(this.email, this.password)
+    .then((res)=>{
+      console.log('Good bien !!');
+      console.log(res);
+    }).catch((err)=>{
+      console.log(err);
+    });
+
+
   }
 
 }
